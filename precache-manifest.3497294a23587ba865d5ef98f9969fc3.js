e7d19fbb82812536c9533b433e0fc947bae2a471
indranil2507 <iindranilsaha1@gmail.com>
2020-07-20 20:38:36 +0530

    Updates

---
+++ precache-manifest.3497294a23587ba865d5ef98f9969fc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1299f43f0f56fc005831ff46a2b5c84",
+    "url": "/movie-database-react/index.html"
+  },
+  {
+    "revision": "c2d0d10c2e205ae4d1c3",
+    "url": "/movie-database-react/static/css/main.af3e5a35.chunk.css"
+  },
+  {
+    "revision": "4a0452f5429935235da0",
+    "url": "/movie-database-react/static/js/2.b6cc2539.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-database-react/static/js/2.b6cc2539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d0d10c2e205ae4d1c3",
+    "url": "/movie-database-react/static/js/main.d352c1bb.chunk.js"
+  },
+  {
+    "revision": "523db2b0901699db6820",
+    "url": "/movie-database-react/static/js/runtime-main.cc4f350f.js"
+  }
+]);
